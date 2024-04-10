d061a6ace6f88dfaa6c71baeb5c92325f804f764
Zac Mohamoud <zac.mohamoud@digital.cabinet-office.gov.uk>
2024-04-10 11:30:24 +0100

    update run-tests.sh to run check /resume shell script

--- run-tests.sh
+++ run-tests.sh
@@ -2,31 +2,20 @@
 
 set -eu
 
-# # The CFN variables seem to include quotes when used in tests these quotes must
-# # be removed before assigning these variable.
-# remove_quotes () {
-#   echo "$1" | tr -d '"'
-# }
+# The CFN variables seem to include quotes when used in tests these quotes must
+# be removed before assigning these variable.
+remove_quotes () {
+  echo "$1" | tr -d '"'
+}
 
-# # Github actions set to true for tests to run in headless mode
-# export GITHUB_ACTIONS=true
-# # shellcheck disable=SC2154
-# export API_BASE_URL=$(remove_quotes "$CFN_BAVBackEndURL")
-# export IPV_STUB_URL=$(remove_quotes $CFN_BAVIPVStubPrettyAPIURL)start
-# export TEST_HARNESS_URL=$(remove_quotes $CFN_BAVTestHarnessURL)
-# export SESSION_TABLE=$(remove_quotes $CFN_BAVBackendSessionTableName)
+# Github actions set to true for tests to run in headless mode
+export GITHUB_ACTIONS=true
+# shellcheck disable=SC2154
+export IPR_CUSTOM_DOMAIN=$(remove_quotes "$CFN_IPRCustomDomain")
 
-# declare error_code
+declare error_code
 
-# cd /app; yarn run test:e2e:cd
-# error_code=$?
+cd /app; npm run test:ipvr https://$IPR_CUSTOM_DOMAIN/resume
+error_code=$?
 
-# cp -rf /app/test/reports $TEST_REPORT_ABSOLUTE_DIR
-
-# sleep 2m
-
-# apt-get install jq -y
-# cd /app; npm run test:pii
-# error_code=$?
-
-# exit $error_code
+exit $error_code
