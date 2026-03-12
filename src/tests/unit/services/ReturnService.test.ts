4e98b98642ea90272377f159e96dcebb6cc51f06
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2026-03-12 11:10:34 +0000

    Delete session test added

--- src/tests/unit/services/ReturnService.test.ts
+++ src/tests/unit/services/ReturnService.test.ts
@@ -1,6 +1,7 @@
 import { ReturnService } from "../../../services/ReturnService";
 import { createDynamoDbClient } from "../../../utils/DynamoDBFactory";
 import { EnvironmentVariables } from "../../../utils/EnvironmentVariables";
+import { loggingHelper } from "../../../utils/LoggingHelper";
 
 let returnService: ReturnService;
 
@@ -15,10 +16,17 @@ describe("ReturnService test", () => {
 		returnService = new ReturnService(EnvironmentVariables.getSessionTableName(), mockDynamoDbClient);
 	});
 
-	it("successfully save session", async () => {
-
+	it("successfully saves session", async () => {
 		mockDynamoDbClient.send = jest.fn().mockResolvedValue({});
 		const actualResult = await returnService.createSession();
 		expect(actualResult).toMatch(/^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i);
+	});
+
+	it("successfully deletes session", async () => {
+		mockDynamoDbClient.send = jest.fn().mockResolvedValue({});
+		await returnService.createSession();
+		const actualResult = await returnService.deleteSession("test");
+		expect(actualResult).toEqual(undefined);
+		expect(loggingHelper.info).toHaveBeenCalled;
 	});
 });
