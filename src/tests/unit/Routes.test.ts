609fb70e8d6a0b947ece8171e2644796b198c12f
George Dainton <george.dainton@digital.cabinet-office.gov.uk>
2026-03-17 16:54:22 +0000

    Cleanup

--- src/tests/unit/Routes.test.ts
+++ src/tests/unit/Routes.test.ts
@@ -1,10 +1,4 @@
-import { EnvironmentVariables } from "../../utils/EnvironmentVariables";
-import express, { Router } from "express";
-
-import * as Routes from "../../Routes";
-import { register } from "../../Routes";
 import index from "../../Index";
-import { loggingHelper } from "../../utils/LoggingHelper"; 
 const request = require('supertest')
 
 describe("Routes", () => {
